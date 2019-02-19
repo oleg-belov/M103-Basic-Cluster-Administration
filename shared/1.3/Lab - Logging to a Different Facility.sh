ac0e9bc5a373393dec9d7ff5c54367b677a708e0
Oleg Belov <oleg.s.belov@gmail.com>
2019-02-19 16:12:59 +0200

    Complete first week

---
+++ shared/1.3/Lab - Logging to a Different Facility.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# ============================================================================== #
+# MIT License                                                                    #
+#                                                                                #
+# Copyright (c) 2019 Oleg Belov                                                  #
+#                                                                                #
+# Permission is hereby granted, free of charge, to any person obtaining a copy   #
+# of this software and associated documentation files (the "Software"), to deal  #
+# in the Software without restriction, including without limitation the rights   #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      #
+# copies of the Software, and to permit persons to whom the Software is          #
+# furnished to do so, subject to the following conditions:                       #
+#                                                                                #
+# The above copyright notice and this permission notice shall be included in     #
+# all copies or substantial portions of the Software.                            #
+#                                                                                #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #
+# SOFTWARE.                                                                      #
+# ============================================================================== #
+#                                                                                #
+# DESCRIPTION : Solution for M103-Basic-Cluster-Administration's Lab 4.          #
+# AUTHOR : Oleg Belov                                                            #
+# COPYRIGHT : Copyright (c) 2019 Oleg Belov                                      #
+# LICENSE : MIT                                                                  #
+#                                                                                #
+# ============================================================================== #
+
+# Print start status message.
+CURRENT_DATE=$(date)
+echo Start: ${CURRENT_DATE}
+
+# Kills all the mongo process and waits for them to exit.
+pkill mongod
+sleep 3
+
+# Create the directory
+sudo mkdir -p /var/mongodb/db/
+
+# Add rights to the directory
+sudo chmod 777 /var/mongodb/db/
+  
+# Start the server
+mongod -f ./mongod.conf
+
+# Create admin user & hide the output
+mongo admin --host localhost:27000 --eval '
+  db.createUser({
+    user: "m103-admin",
+    pwd: "m103-pass",
+    roles: [
+      {role: "root", db: "admin"}
+    ]
+  })' >&- 2>&-
+
+echo "Admin user is created!"
+
+# Run Test
+RESULT=$(validate_lab_different_logpath)
+
+# Print the result
+echo "The reult of the test = ${RESULT}"
